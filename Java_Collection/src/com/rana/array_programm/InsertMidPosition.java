ca1e299f0805424c41f2db7db23d9358a2cb2b2e
ranap <ranap@LAPTOP-CC5GVBOT>
2026-02-23 17:37:47 +0530

    Mid position i

--- Java_Collection/src/com/rana/array_programm/InsertMidPosition.java
+++ Java_Collection/src/com/rana/array_programm/InsertMidPosition.java
@@ -1,10 +1,12 @@
 package com.rana.array_programm;
+//Q6) Insert a new element in the array at middle position
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class InsertMidPosition {
 
+	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) 
 	{
 	Scanner sc=new Scanner(System.in);
@@ -21,12 +23,25 @@ public class InsertMidPosition {
 		}
 		
 		int mid=(array.length)/2;
+		int[] newArray=new int[size+1];
 		
-		for (int i = 0;i<=mid; i++) 
+		for (int i = 0;i<mid; i++) 
 		{
-			
+			newArray[i]=array[i];
 		}
-
+		newArray[mid]=13;
+		
+		for (int i = mid; i < size; i++) 
+		{
+			newArray[i+1]=array[i];
+		}
+		
+		System.out.println("After insertion");
+		for (int i : newArray) 
+		{
+			System.out.println(i);
+		}
+		
 	}
 
 }
